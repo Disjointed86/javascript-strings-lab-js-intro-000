cddfcb79b6f27afbbc1b640ef6e056442bdcdce4
Trevis Day <trevisday@gmail.com>
2018-01-29 05:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = greetingIntro + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk about space";
 
 var conversation = `${topic}`;
